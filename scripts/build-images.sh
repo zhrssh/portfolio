dcf4333d5a057f664aed5c416b8617ce57df9d55
Zherish Galvin Mayordo <98146850+zhrssh@users.noreply.github.com>
2026-03-25 00:33:17 +0800

    Added docker compose + Updated config

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-NAMESPACE="portfolio"
 ROOTDIR=".."
 WEBCLIENT_DIR="$ROOTDIR/src/webclient"
 
 if [[ -d $WEBCLIENT_DIR ]] && command -v docker &>/dev/null; then
     echo "Building Dockerfile at $WEBCLIENT_DIR"
     cd $WEBCLIENT_DIR
-    docker build . -t $NAMESPACE/webclient
+    docker build . -t webclient
 fi
